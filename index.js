551913312af40972b5ffafbdb5ea1a88378ea2ec
Jose Laurito <jigolaurito93@yahoo.com>
2023-08-21 03:57:18 -0500

    created navbar and added icons from fontawesome, generated EJS and included partials, navbar.ejs

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ import ejs from 'ejs';
 const app = express();
 const port = 3000
 
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.render('index.ejs');
+})
+
 app.listen(port, () =>{
     console.log(`listening on port ${port}`);
 })
