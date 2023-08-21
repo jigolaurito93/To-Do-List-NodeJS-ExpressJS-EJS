3b05d66f7d5b931a31e8c45bd5528b1901c2c2a2
Jose Laurito <jigolaurito93@yahoo.com>
2023-08-21 03:31:28 -0500

    set up project

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Hello world!");
+import express from 'express';
+import ejs from 'ejs';
+
+const app = express();
+const port = 3000
+
+app.listen(port, () =>{
+    console.log(`listening on port ${port}`);
+})
